0382cb06c58225fdfed3625feae9668c723c2656
Aditya Kumar <adkum@microsoft>
2021-08-08 23:47:49 +0530

    Add UT

--- app/src/test/java/com/aditya/github/DateUtilsTest.kt
+++ app/src/test/java/com/aditya/github/DateUtilsTest.kt
@@ -1,5 +1,7 @@
 package com.aditya.github
 
+import com.aditya.github.utils.DateUtils
+import org.junit.Assert
 import org.junit.Test
 
 /**
@@ -7,6 +9,7 @@ import org.junit.Test
  */
 class DateUtilsTest {
     @Test
-    fun addition_isCorrect() {
+    fun getFormattedDate() {
+        Assert.assertEquals("2020-02-29", DateUtils.getFormattedDate("2020-02-29T17:36:40Z"))
     }
 }
